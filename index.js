d29849c3107c12cc2e39d9e56af9e900e53045a6
Rasha <badranrasha685@gmail.com>
2023-01-19 13:08:47 +0200

    file-structure

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const connection = require("./config/db");
+const express = require("express");
+const app = express();
+mongoose.set("strictQuery", true);
+
+
+connection();
+
+const conn = mongoose.connection;
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
