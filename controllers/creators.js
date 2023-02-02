5b74302c48bba56f0d73ecbbfb292536fa800660
faysal-sardouk <sardouk29@gmail.com>
2023-02-02 12:05:31 +0200

    backend

--- controllers/creators.js
+++ controllers/creators.js
@@ -1,33 +1,37 @@
 //to code for the creators
 const asyncHandler = require("express-async-handler");
 const Creator = require("../models/creators");
-const multer=require('multer');
-const fs=require('fs');
-const path=require('path');
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 const { error } = require("console");
 //@desc Get Creator (read=>post)
 // @route Get /api/Creators
 // @access Private
 
-const storage=multer.diskStorage({
-  fileFilter: function (req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png|svg)$/)) {
-      return cb(new Error("Unexpected field"), false);
-    }
-    cb(null, true);
-  },
-  destination:"creators",
-  filename:(req,file,cb)=>{
-      cb(null,file.originalname);
+// const storage = multer.diskStorage({
+//   destination: "uploadss/",
+//   filename: (req, file, cb) => {
+//     console.log("zeinab ", req.files);
+//     cb(null, file);
+//   },
+// });
+// const upload = multer({
+//   storage: storage,
+// });
 
+const storage = multer.diskStorage({
+destination: function(req, file, cb) {
+  cb(null, "./uploadss");
+},
+filename: function(req, file, cb) {
+  cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
 }
+});
 
-}
-)
-const uploadss=multer({
-  storage:storage
-})
-
+const upload = multer({
+  storage: storage
+});
 
 const getCreator = asyncHandler(async (req, res) => {
   const creators = await Creator.find();
@@ -40,23 +44,29 @@ const getCreator = asyncHandler(async (req, res) => {
 // @access Private
 
 const setCreator = asyncHandler(async (req, res) => {
+  console.log("req ", req.files);
   if (!req.body) {
     res.status(400);
+    console.log("error123", error);
     throw new Error("Please add a set creator");
-    console.log(error)
   }
+
+  console.log("ALOOOO ",req.files.creatorimg[0].path)
+
   const theCreator = await Creator.create({
     creatorName: req.body.creatorName,
     description: req.body.description,
     creatorimg: {
-      data:req.file.path,
-      contentType:"image.jpg" || "image.png" || "image.svg" || "image.jpeg" ,
-  },
-  bckgrndimg:{
-    data:req.file.path,
-    contentType:"image.jpg" || "image.png" || "image.svg" || "image.jpeg" ,
-  }
+      data: req.files.creatorimg[0].path,
+      contentType: "image.jpg" || "image.png" || "image.svg" || "image.jpeg",
+    },
+
+     bckgrndimg: {
+       data: req.files.bckgrndimg[0].path,
+      contentType: "image.jpg" || "image.png" || "image.svg" || "image.jpeg",
+     },
   });
+
   res.status(200).json(theCreator);
 });
 
@@ -65,15 +75,19 @@ const setCreator = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateCreator = asyncHandler(async (req, res) => {
-  const theCreator = await Creator.findById(req.params.id)
-  if(!theCreator){
-    res.status(400)
-    throw new Error('Update creator is not found')
+  const theCreator = await Creator.findById(req.params.id);
+  if (!theCreator) {
+    res.status(400);
+    throw new Error("Update creator is not found");
   }
-  const updatedCreator = await theCreator.findByIdAndUpdate(req.params.id , req.body,{
-    new: true,
-  })
-  res.status(200).json(updateCreator);
+  const updatedCreator1 = await theCreator.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    }
+  );
+  res.status(200).json(updatedCreator1);
 });
 
 //@desc Delete Creator
@@ -81,23 +95,21 @@ const updateCreator = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteCreator = asyncHandler(async (req, res) => {
-  const theCreator = await theCreator.findById(req.params.id)
-   
-  if(!goal) {
-    res.status(400)
-    throw new Error ('Delete goal not found')
+  const theCreator = await theCreator.findById(req.params.id);
 
+  if (!goal) {
+    res.status(400);
+    throw new Error("Delete goal not found");
   }
-  await goal.remove()
+  await goal.remove();
 
-  res.status(200).json({ id: req.params.id })
-  
-})
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
   getCreator,
   setCreator,
   updateCreator,
   deleteCreator,
-  uploadss,
+  upload,
 };
