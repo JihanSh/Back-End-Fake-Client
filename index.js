af7851ef23b734f2ad3f2c02a3d49f81fd57729b
mgoyeghiaian <mgoyeghiaian@gmail.com>
2023-02-02 15:50:54 +0200

    Fixed Backend

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
-const dotenv = require("dotenv")
-dotenv.config();
+require("dotenv").config();
 const mongoose = require("mongoose");
 const connection = require("./config/db");
 const express = require("express");
 const app = express();
-const faqRoutes = require("./routes/faq");
+const AdminRoute = require("./routes/admin")
+const cors = require('cors')
 mongoose.set("strictQuery", true);
-const cors = require("cors");
-const AdminRoute = require('./routes/admin')
-const nftRoute = require("./routes/nft-collection")
-
-const multer = require("multer");
-
+app.use(cors());
 
 connection();
-const conn = mongoose.connection;
 
 app.use(express.json());
-app.use(faqRoutes);
-app.use(express.urlencoded({ extended: false }));
 
-app.use(AdminRoute)
+app.use(AdminRoute);
 
-app.use("/nft", nftRoute)
-app.use(cors());
+const conn = mongoose.connection;
+
 const port = process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`));
-
-// /nft/postnft
